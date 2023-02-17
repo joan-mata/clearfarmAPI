140560b635848590f6c8507135a2f2a70a57144f
joan-mata <joan.mata.jmp@gmail.com>
2023-02-17 19:24:24 +0100

    evaluate input data pruebas

--- app/inserts/resources/farmPOST.py
+++ app/inserts/resources/farmPOST.py
@@ -33,7 +33,8 @@ def farmPOST():
                 
         data = []
         with open(csvFilePath, encoding='utf-8') as csvf:
-            csvReader = csv.DictReader(csvf)
+            dialect = csv.Sniffer().sniff(csvf.read(1024)) #probar
+            csvReader = csv.DictReader(csvf, dialect.delimiter(";")) #probar
             #add date from today
             date = datetime.today().strftime('%Y-%m-%d')
             dict = {'date_insert_in_db': date}
