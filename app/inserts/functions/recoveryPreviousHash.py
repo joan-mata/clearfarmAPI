26fc750220b73cb8379071c6c0dd87e93d4dfa2f
joan-mata <joan.mata.jmp@gmail.com>
2023-02-17 17:25:41 +0100

    hash previous save&recovery 3

--- app/inserts/functions/recoveryPreviousHash.py
+++ app/inserts/functions/recoveryPreviousHash.py
@@ -11,12 +11,12 @@ from app import db, UPLOAD_FOLDER
 
 def recoveryPreviousHash(dataBase):
     data = list(dataBase.find({}, {"hash": 1}).sort("$natural", -1))
+    #data = list(dataBase.find({}, {"hash": 1}).sort("$natural", -1)).limit(1)
     
     if data:
-        print('Data[0]: ' + str(data[0])+'\n')
         dict_aux = data[0]
-        print('Dict_aux["hash"]: ' + str(dict_aux["hash"])+'\n')
         dict = {'hash_previous': str(dict_aux["hash"])}
+        #dict = {'hash_previous': str(data["hash"])}
     else:
         dict = {'hash_previous': '0'}
 
